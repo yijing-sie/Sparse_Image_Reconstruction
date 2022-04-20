1a493eb0d56743cd5cfd75dc703086791b95ddac
Gkao03 <gorek@andrew.cmu.edu>
2022-04-20 00:45:15 -0400

    image and mask

--- iht/iht.py
+++ iht/iht.py
@@ -5,6 +5,7 @@ from PIL import Image
 import numpy as np
 import pywt
 import scipy.io
+import cv2
 
 
 def imagepath2numpy(im_pth):
@@ -17,6 +18,9 @@ def main(args):
     img_arr = imagepath2numpy(args.image_pth)
     mask = scipy.io.loadmat(args.mask_pth)['mask']
 
+    # resize to args.resolution
+    img_resize = cv2.resize(img_arr, (args.resolution, args.resolution), interpolation=cv2.INTER_CUBIC)
+
 
 def parse_arg():
     """Creates a parser for command-line arguments.
@@ -25,6 +29,7 @@ def parse_arg():
     
     parser.add_argument('--image_pth', type=str, default='data/image.png', help="path to the input image")
     parser.add_argument('--mask_pth', type=str, default='mask.mat', help="path to mask")
+    parser.add_argument('--resolution', type=int, default=600, help="resolution of images to be resized to")
     return parser.parse_args()
 
 if __name__ == '__main__':
