18278a7ca63820b87418832e3e4a64e666219e08
Gkao03 <gorek@andrew.cmu.edu>
2022-04-25 21:49:22 -0400

    use sklearn mse

--- iht/utils.py
+++ iht/utils.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import numpy as np
+from sklearn.metrics import mean_squared_error
 
 
 def imagepath2numpy(im_pth):
@@ -14,11 +15,7 @@ def save_nparray2image(arr, fpath):
 
 def calc_mse(arr1, arr2):
     assert(arr1.shape == arr2.shape)
-    sub = arr1 - arr2
-    squared = np.square(sub)
-    mse = np.sum(squared) / squared.size
-
-    return mse
+    return mean_squared_error(arr1, arr2)
 
 def normalize01(arr):
     return (arr - np.min(arr)) / np.ptp(arr)
